f3ec15a6323f6ee354df6a4f154df2254378b368
MiketoCoding <le.michael.v@gmail.com>
2021-11-02 14:24:19 -0400

    Update SignUp component
    Working signup component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 import SignUp from './components/SignUp/SignUp'
 
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from 'react'
-import { withRouter } from 'react-router-dom'
+// import { withRouter } from 'react-router-dom'
 
-import { signUp } from '../../api/auth'
+import { signUp, signIn } from '../../api/auth'
+import messages from '../AutoDismissAlert/messages'
 
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
@@ -17,20 +18,31 @@ class SignUp extends Component {
         }
     }
 
-    handlechange = event => this.setState({
+    handleChange = event => this.setState({
         [event.target.name]: event.target.value
     })
 
     onSignUp = event => {
         event.preventDefault()
 
-        const { history, setUser } = this.props
+        const { msgAlert, history, setUser } = this.props
 
         signUp(this.state)
+            .then(() => signIn(this.state))
             .then(res => setUser(res.data.user))
+            .then(() => msgAlert({
+                heading: 'Sign Up Success',
+                message: messages.signUpSuccess,
+                variant: 'success'
+            }))
             .then(() => history.push('/'))
             .catch(error => {
                 this.setState({ email: '', password: '', passwordConfirmation: ''})
+                msgAlert({
+                    heading: 'Sign Up Failed with error: ' + error.message,
+                    message: messages.signUpFailure,
+                    variant: 'danger'
+                })
             })
     }
 
@@ -38,7 +50,8 @@ class SignUp extends Component {
         const { email, password, passwordConfirmation } = this.state
 
         return (
-            <Fragment>
+            <div>
+            <div>
                 <h3>Sign Up</h3>
                 <Form onSubmit={this.onSignUp}>
                     <Form.Group controlId="email">
@@ -48,7 +61,7 @@ class SignUp extends Component {
                             type="email"
                             name="email"
                             value={email}
-                            placeholder="enter email"
+                            placeholder="Enter email"
                             onChange={this.handleChange}
                         />
                     </Form.Group>
@@ -57,7 +70,7 @@ class SignUp extends Component {
                         <Form.Control
                             required
                             name="password"
-                            value="password"
+                            value={password}
                             placeholder="Password"
                             onChange={this.handleChange}
                         />
@@ -70,9 +83,8 @@ class SignUp extends Component {
                             value={passwordConfirmation}
                             type="password"
                             placeholder="ConfirmPassword"
-                            onChange={this.handlechange}
+                            onChange={this.handleChange}
                         />
-
                     </Form.Group>
                     <Button
                         variant="primary"
@@ -81,7 +93,8 @@ class SignUp extends Component {
                         Submit 
                     </Button> 
                 </Form>
-            </Fragment>
+            </div>
+            </div>
         )
     }
 }
