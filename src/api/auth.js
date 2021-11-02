92c235ed39762e7cee00e51139d285081a82bdc7
alaskaThunderfx <scott.thomas.mackey@gmail.com>
2021-11-02 11:52:23 -0400

    Add auth sign in/out, change password

--- src/api/auth.js
+++ src/api/auth.js
@@ -14,3 +14,42 @@ export const signUp = credentials => {
         }
     })
 }
+
+export const signIn = credentials => {
+    return axios({
+        url: apiUrl + '/sign-in',
+        method: 'POST',
+        data: {
+            credentials: {
+                email: credentials.email,
+                password: credentials.password
+            }
+        }
+    })
+}
+
+export const signOut = user => {
+    return axios({
+        url: apiUrl + '/sign-out',
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${user.token}`
+        }
+    })
+}
+
+export const changePassword = (passwords, user) => {
+    return axios({
+        url: apiUrl + '/change-password',
+        method : 'PATCH',
+        headers: {
+            'Authorization': `Bearer ${user.token}`
+        },
+        data: {
+            passwords: {
+                old: passwords.oldPassword,
+                new: passwords.newPassword
+            }
+        }
+    })
+}
